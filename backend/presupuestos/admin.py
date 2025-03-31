e491990d04c36d4368d3ffcad0805e6e08e71159
Matias <matias.rojas0852@protonmail.com>
2025-03-31 08:43:56 -0300

    Presupuesto y pago registrados en el admin

--- backend/presupuestos/admin.py
+++ backend/presupuestos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+from .models import Presupuesto, Pago
+
+@admin.register(Presupuesto)
+class PresupuestoAdmin(ModelAdmin):
+    list_display = ("id", "cliente", "monto_total", "estado", "fecha_creacion")
+    list_filter = ("estado", "fecha_creacion")
+    search_fields = ("cliente__nombre",)
+
+@admin.register(Pago)
+class PagoAdmin(ModelAdmin):
+    list_display = ("presupuesto", "monto", "fecha", "metodo")
+    list_filter = ("metodo", "fecha")
